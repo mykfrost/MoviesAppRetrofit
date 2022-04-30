ebe662a8f5949495318e5d94e96c06e4fb50faf5
Incredible Mike Frost <okellofrost@gmail.com>
2022-04-30 19:42:20 +0300

    My first retrofit app using REST API for Movies

---
+++ app/src/main/java/com/example/retrofit/MainPage/List_item_movie.java
@@ -0,0 +1,16 @@
+package com.example.retrofit.MainPage;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.retrofit.R;
+
+public class List_item_movie extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.list_item_movie);
+    }
+}
---
+++ app/src/main/java/com/example/retrofit/MainPage/MainActivity.java
@@ -0,0 +1,63 @@
+package com.example.retrofit.MainPage;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.example.retrofit.Helpers.ApiClient;
+import com.example.retrofit.Helpers.ApiInterface;
+import com.example.retrofit.Helpers.Movie;
+import com.example.retrofit.Helpers.MovieResponse;
+import com.example.retrofit.Helpers.MoviesAdapter;
+import com.example.retrofit.R;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+    //  - insert your themoviedb.org API KEY here
+    private final static String API_KEY = "fa21a809ae915d168cb53826b25cce67";
+    MoviesAdapter adapter;
+    RecyclerView recycler;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        if (API_KEY.isEmpty()) {
+            Toast.makeText(getApplicationContext(), "Please obtain your API KEY first from themoviedb.org", Toast.LENGTH_LONG).show();
+            return;
+        }
+        recycler = findViewById(R.id.movies_recycler_view);
+        recycler.setLayoutManager(new LinearLayoutManager(this));
+
+        ApiInterface apiService = ApiClient.getClient().create(ApiInterface.class);
+
+        Call<MovieResponse> call = apiService.getTopRatedMovies(API_KEY);
+        call.enqueue(new Callback<MovieResponse>() {
+            @Override
+            public void onResponse(Call<MovieResponse> call, Response<MovieResponse> response) {
+                int statusCode = response.code();
+                List<Movie> movies = response.body().getResults();
+                recycler.setAdapter(new MoviesAdapter(movies, R.layout.list_item_movie, getApplicationContext()));
+            }
+
+            @Override
+            public void onFailure(Call<MovieResponse> call, Throwable t) {
+                // Log error here since request failed
+                Log.e(TAG, t.toString());
+            }
+        });
+
+
+    }
+}
